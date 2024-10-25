e76e62c734f87b6f9f38350027c5258cf1deac77
ulrich hamaani <119246440+ulrichhamaani237@users.noreply.github.com>
2024-10-25 12:41:20 +0100

    form3D

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+function rotateForm() {
+   
+    let formCard = document.querySelector('.form-animated-card')
+
+   formCard.classList.add('rotate180')
+  
+
+}
+
+function rotateFormBack(){
+    let formCard = document.querySelector('.form-animated-card')
+
+   formCard.classList.remove('rotate180')
+}
+
+
